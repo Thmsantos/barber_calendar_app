5911d2ebb035b8a7d5b5b60c2439dca410b76171
thmsantos <thiagomessias411@gmail.com>
2026-04-09 11:33:48 -0300

    :sparkles: feat: create barber page

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Presentation from "../../pages/Presentation";
+import BarberPage from "../../pages/BarberPage";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Presentation />
+    },
+    {
+        path: "/barber/:id",
+        element: <BarberPage />
     }
 ])
